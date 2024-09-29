c8dbc62ecbb446d89075cec57c28c0aa53efebea
Chinedu-BN <chinwochinedu2024@gmail.com>
2024-09-29 13:37:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-const express = require('express');
-const app = express();
-__path = process.cwd()
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8000;
-let code = require('./pair');
-require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/trinity.html')
-})
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log(`YoutTube: @DElite-Pro-Empire\nTelegram: ElitePro_md\nGitHub: @Elite-Pro-V2\nAuthor: Chinedu-MD\n\nServer running on http://localhost:` + PORT)
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send(_path +'/trinity.html')
 })
 
-module.exports = app
+app.listen(port, () => {
+    console.log(`Server is listening on the port ${port}.`)
+})
